9b08b53e525d47b00ce87a101d81faf15ccd1f7f
razvan2004 <42996838+razvan2004@users.noreply.github.com>
2023-03-27 00:57:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@
 #include <unistd.h>
 
 #define TIMEDELAY 100000
-int noIterations=100000;
+int noIterations=10000;
 float initialEnergy;
+Vector3 initialMomentum;
 std::vector<planet> bodies;
 
 
@@ -14,17 +15,26 @@ int main()
 {
     generatePlanets(bodies,50,100000,10000,500);
     initialEnergy=totalEnergy(bodies);
-    std::cout<<initialEnergy;
+    assignVector(initialMomentum,totalMomentum(bodies));
+    std::cout<<initialEnergy<<" ";
+    displayVector(initialMomentum);
     //physics
     while(noIterations--)
     {
         for(int i=0; i<bodies.size(); i++)
         {
-            bodies[i].updateacc(bodies);
-            bodies[i].updatevel();
-            bodies[i].updatepoz();
-            if(noIterations%2500==0)
-                o<<totalEnergy(bodies)/initialEnergy<<'\n';
+            bodies[i].updatePhysics(bodies);
+
+        }
+        if(noIterations%10==0)
+        {
+            Vector3 momentum;
+            momentum.assignValue(0,0,0);
+            o<<std::fixed<<std::setprecision(10)<<totalEnergy(bodies)/initialEnergy<<' ';
+            addVectors(momentum,rescale(totalMomentum(bodies),-1));
+            addVectors(momentum,initialMomentum);
+            saveWholeVector(momentum);
+            o<<"\n";
         }
     }
     exit(EXIT_SUCCESS);
